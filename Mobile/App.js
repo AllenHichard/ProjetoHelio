d14958c431f943901275c0faf0b4f0d3df9a630a
unknown <allenhichard>
2020-02-04 20:14:47 -0300

    Inicializando o projeto de Hélio

---
+++ Mobile/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Word</Text>
+      <Button style = {styles.button} title= "Clicou"></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#fff',
+  }
+});
